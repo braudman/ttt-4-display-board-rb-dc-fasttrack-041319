3095c0c3e2b7db4234b5545e663b61d4c583c4ba
Stephen M Braudo <braudman@gmail.com>
2019-04-13 19:29:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-bd =["O"," "," "," ","X"," "," "," ",]
+board =["O"," "," "," ","X"," "," "," ",]
 def display_board(board)
-  puts "#{bd[0]} |   |   "
+  puts "#{board[0]} |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
